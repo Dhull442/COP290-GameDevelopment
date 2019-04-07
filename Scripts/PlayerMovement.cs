6e3d68eaf7a47fff7af428ea75f5ac2e0cc838ba
root <root@DESKTOP-PR20BPN.localdomain>
2019-04-08 01:18:43 +0530

    Player done

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+     
+    public float speed =1.0f;
+    public string axis1 = "Horizontal";
+    public string axis2 = "Vertical";
+    // public Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        // anim = gameObject.GetComponent<Animator> ();
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+        // anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis(axisName)));
+        if (Input.GetAxis (axis1) < 0)
+        {
+                Vector3 newScale = transform.localScale;
+                newScale.y = 1.0f;
+                newScale.x = 1.0f;
+                transform.localScale = newScale;
+        } 
+        else if (Input.GetAxis (axis1) > 0)
+        {
+                Vector3 newScale =transform.localScale;
+                newScale.x = 1.0f;
+                transform.localScale = newScale;        
+        }
+        if (Input.GetAxis (axis2) < 0)
+        {
+                Vector3 newScale = transform.localScale;
+                newScale.y = 1.0f;
+                newScale.x = 1.0f;
+                transform.localScale = newScale;
+        } 
+        else if (Input.GetAxis (axis2) > 0)
+        {
+                Vector3 newScale =transform.localScale;
+                newScale.y = 1.0f;
+                transform.localScale = newScale;        
+        }
+
+        transform.position += (transform.right *Input.GetAxis(axis1) + transform.up *Input.GetAxis(axis2))* speed * Time.deltaTime;
+
+    }
+}
---
+++ Scripts/StickMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickMovement : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+    public GameObject parent;
+    public float stickLength = 1.0f;
+    float centerx = Screen.width/2.0f, centery = Screen.height/2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Console.WriteLine(Input.mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   float x,y;
+        System.Console.WriteLine(Input.mousePosition);
+        x = Input.mousePosition.x - centerx;
+        y = Input.mousePosition.y - centery;
+         if (Input.GetButton("Fire1")){
+             float t = Time.deltaTime;
+             float d = Vector3.Distance(transform.position + (transform.right *(x)/centerx + transform.up *(y)/centery)* rotationSpeed * t,parent.transform.position); 
+             if((d) < stickLength )
+                {transform.position += (transform.right *(x)/centerx + transform.up *(y)/centery)* rotationSpeed * t;
+                Debug.Log("Position Changing ");
+                Debug.Log(d);
+         }
+         }
+    }
+
+    // float distance(Vector3 diff){
+    // return Math.Sqrt(
+    // Math.Pow(diff.x, 2f) +
+    // Math.Pow(diff.y, 2f) +
+    // Math.Pow(diff.z, 2f));
+    // }
+}
