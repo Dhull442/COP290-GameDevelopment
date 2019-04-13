85dab817910652f1833279c1f9b14146c0a249e7
Saksham <sakshamdhull442@gmail.com>
2019-04-13 15:13:55 +0530

    improved stick movement with mouse

--- Scripts/StickMovement.cs
+++ Scripts/StickMovement.cs
@@ -18,14 +18,40 @@ public class StickMovement : MonoBehaviour
     void Update()
     {   float x,y;
         System.Console.WriteLine(Input.mousePosition);
-        x = Input.mousePosition.x - centerx;
-        y = Input.mousePosition.y - centery;
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,0);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        x = objPosition.x - transform.position.x;
+        y = objPosition.y - transform.position.y;
+        Debug.Log(x +" "+y);
         float t = Time.deltaTime;
-        float d = Vector3.Distance(transform.position + (transform.right *(x)/centerx + transform.up *(y)/centery)* rotationSpeed * t,parent.transform.position); 
+        float movex = 0.0f, movey=0.0f;
+        if((x > 0.05f || x < -0.05f) && (y > 0.05f || y < -0.05f)){
+            float mag = Mathf.Sqrt(x*x + y*y);
+            movex = x/mag;
+            movey = y/mag;
+        }
+        else{ if ((x > 0.05f || x < -0.05f)){
+            if( x>0){
+                movex = 1.0f;
+            }
+            if(x < 0){
+                movex = -1.0f;
+            }
+        }
+        if ((y > 0.05f || y < -0.05f)){
+            if( y>0){
+                movey = 1.0f;
+            }
+            if(y < 0){
+                movey = -1.0f;
+            }
+        }}
+        
+        float d = Vector3.Distance(transform.position + (transform.right *(movex) + transform.up *(movey))* rotationSpeed * t,parent.transform.position); 
              
         if (Input.GetButton("Fire1")){
             if((d) < stickLength )
-            {transform.position += (transform.right *(x)/centerx + transform.up *(y)/centery)* rotationSpeed * t;}
+            {transform.position += (transform.right *(movex) + transform.up *movey )* rotationSpeed * t;}
         }
         if(Input.GetButton("Fire2")){
             if(transform.position.x - parent.transform.position.x > safeBoundary){
