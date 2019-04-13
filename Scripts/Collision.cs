b98b17500fcb5d31a058574108e003b9c214160b
Saksham <sakshamdhull442@gmail.com>
2019-04-13 15:13:40 +0530

    added more audio

--- Scripts/Collision.cs
+++ Scripts/Collision.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {   
+
+    AudioSource source;
+    public AudioClip[] clips;
+    public AudioClip[] stickclips;
     public Material[] materials;
     float timeOfCollision = -100.0f;
     public float timeout = 1.0f;
     Renderer rend;
     void OnCollisionEnter(UnityEngine.Collision coll){
         if(coll.gameObject.tag == "stick"){
+            source.clip = stickclips[Random.Range(0,stickclips.Length -1)];
+            source.Play();
             Debug.Log("Collision with stick");
             rend.sharedMaterial = materials[1];
             timeOfCollision = Time.time;
         }
+        if(!source.isPlaying){
+            source.clip = clips[Random.Range(0,clips.Length -1 )];
+            source.Play();
+        }
+        
     }
     // Start is called before the first frame update
     void Start()
@@ -21,6 +32,7 @@ public class Collision : MonoBehaviour
         rend = GetComponent<Renderer>();
         rend.enabled = true;
         rend.sharedMaterial = materials[0];
+        source = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
--- todo.md
+++ todo.md
@@ -2,6 +2,9 @@
 - Can smoothen out the movement of stick and can add more control using mouse.
 - addition of sounds to the program.
 - Improve sound sourcing.
+- walking trail.
+- fading color.
+- maybe healthbar and cutters in the way.
 - addition of more obstacles.
 [x] bouncing off on colliding with boundary.
 - background music.
