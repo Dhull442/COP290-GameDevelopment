fc6fefedb3462192870f26d2d11bbf9352fddc45
Saksham <sakshamdhull442@gmail.com>
2019-05-05 23:05:41 +0530

    added more sound effects

--- Scripts/Collision.cs
+++ Scripts/Collision.cs
@@ -6,39 +6,58 @@ public class Collision : MonoBehaviour
 {   
 
     AudioSource source;
+    AudioSource rotationSource;
+
+    AudioSource movementSource;
+
+    AudioSource harmSource;
+
+    AudioSource expansionSource;
     public AudioClip[] clips;
     public AudioClip[] stickclips;
+
+    public AudioClip[] movementClips;
+    public AudioClip[] rotationclips;
+    public AudioClip[] expansionclips;
+    public AudioClip[] harmClips;
+
     public Material[] materials;
-    private Collider coll;
+    // private Collider coll;
     private float timeOfCollision = -100.0f;
     public float timeout = 1.0f;
-    public float timePenalty;
+    public float timePenalty = 1.5f;
     public bool movingObstacle;
-    public float movementAcceleration; // SHM movement
+    public float movementAcceleration = 1.0f; // SHM movement
     private Vector3 centerposition;
-    public float movementBound;
+    public float movementBound = 1.0f;
     public bool VerticalMovement;
     public bool harmfulObstacle;
     public bool expandable;
     private bool expand; // set true then expand to max;
     private float expansionStartTime;
-    public float expandTime;
+    public float expandTime = 5; /// in seconds
     private bool expansionDone=false;
-    public float[] dimLimits; // Dimension Limits for Expansion 
+    public float[] dimLimits= {2.0f,2.0f}; // Dimension Limits for Expansion 
+
+    public bool rotatingObstacle;
+    public float rotationSpeed = 100f;
 
     private Vector3 originalSize;
 
     Renderer rend;
     void OnCollisionEnter(UnityEngine.Collision coll){
         if(coll.gameObject.tag == "stick"){
-            source.clip = stickclips[Random.Range(0,stickclips.Length -1)];
-            source.Play();
-            Debug.Log("Collision with stick");
             if(harmfulObstacle){
+                harmSource.clip = harmClips[Random.Range(0,harmClips.Length-1)];
+                harmSource.Play();
                 rend.sharedMaterial = materials[2];
             }
-            else
-                rend.sharedMaterial = materials[1];
+            else{
+            source.clip = stickclips[Random.Range(0,stickclips.Length -1)];
+            source.Play();
+            rend.sharedMaterial = materials[1];
+            }
+            // Debug.Log("Collision with stick");                
             if(expandable  && !(expansionDone || expand)){
                 expand = true;
                 expansionStartTime = Time.time;
@@ -46,13 +65,18 @@ public class Collision : MonoBehaviour
             timeOfCollision = Time.time;
         }
         if(coll.gameObject.tag=="player"){
-            
-        if(!source.isPlaying){
-            source.clip = clips[Random.Range(0,clips.Length -1 )];
-            source.Play();
-        }
         if(harmfulObstacle){
-            // Time Penalty along with alert!
+            if(!harmSource.isPlaying){
+                harmSource.clip = harmClips[Random.Range(0,harmClips.Length-1)];
+                harmSource.Play();
+            }
+            // Time Penalty here
+        }
+        else{
+            if(!source.isPlaying){
+                source.clip = clips[Random.Range(0,clips.Length -1 )];
+                source.Play();
+            }
         }
         }
         
@@ -63,11 +87,15 @@ public class Collision : MonoBehaviour
         rend = GetComponent<Renderer>();
         rend.enabled = true;
         rend.sharedMaterial = materials[0];
-        source = GetComponent<AudioSource> ();
+        // source = GetComponent<AudioSource> ();
         centerposition = transform.position;
         expand = false;
-        coll = GetComponent<BoxCollider>();
-        originalSize = coll.bounds.size;
+        originalSize =GetComponent<BoxCollider>().bounds.size;
+        source = GetComponents<AudioSource>()[0];
+        expansionSource = GetComponents<AudioSource>()[1];
+        harmSource = GetComponents<AudioSource>()[2];
+        movementSource = GetComponents<AudioSource>()[3];
+        rotationSource = GetComponents<AudioSource>()[4];
     }
 
     // Update is called once per frame
@@ -86,17 +114,32 @@ public class Collision : MonoBehaviour
             }
             float w = Mathf.Sqrt(movementAcceleration / movementBound);
             transform.Translate(centerposition + dir*movementBound*Mathf.Cos(w*Time.time) - transform.position);
+            if(!movementSource.isPlaying){
+                movementSource.clip= movementClips[Random.Range(0,movementClips.Length -1 )];
+                movementSource.Play();
+            }
         }
         if(expand){
             if(Time.time > expansionStartTime + expandTime){
                 expansionDone = true;
                 expand = false;
+                expansionSource.Stop();
             }
             float changeamt = (Time.time - expansionStartTime)/expandTime;
             Vector3 targetscale = (dimLimits[0] -1.0f)*originalSize.x*Vector3.right + (dimLimits[1]-1.0f)*originalSize.y*Vector3.up ;
             
             // add music as well;
             transform.localScale = changeamt* targetscale + originalSize;
+            if(!expansionSource.isPlaying){
+                expansionSource.clip= expansionclips[Random.Range(0,expansionclips.Length -1 )];
+                expansionSource.Play();
+            }
+        }
+        if(rotatingObstacle){
+            transform.Rotate(0,0,rotationSpeed * Time.deltaTime);
+            if(!rotationSource.isPlaying){
+                rotationSource.clip = rotationclips[Random.Range(0,rotationclips.Length -1)];
+            }
         }
     }
 }
