6bd6ece9abe8da5265179f7a9047e73c5dde1ed0
Saksham Dhull <sakshamdhull442@gmail.com>
2019-05-07 21:16:45 +0530

    hints

--- Scripts/CameraMovement.cs
+++ Scripts/CameraMovement.cs
@@ -6,58 +6,38 @@ public class CameraMovement : MonoBehaviour
 {   
     public GameObject player;
     public float cameraSpeed;
-    public int hintNum;
-    private int hintsgiven;
-    public float hintTimeout;
-    private float hinttime;
-    public Color hcolor;
-    public int zoom;
-    public int stdzoom;
-    public Color stdcolor = Color.black;
     private Camera cam;
     public float dimensionX,dimensionY;
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<Camera>();
-        hintsgiven = 0;
-        hinttime = -100;
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if(Input.GetKeyDown(KeyCode.H)){
-            if(hintsgiven < hintNum){
-                cam.backgroundColor = hcolor;
-                cam.orthographicSize = zoom;
-                hinttime = Time.timeSinceLevelLoad;
-                hintsgiven += 1;
-            }
-            else{
-                Debug.Log("Consumed all hints");
-            }
-        }
-        if(Time.timeSinceLevelLoad > hinttime + hintTimeout){
-            cam.orthographicSize = stdzoom;
-            cam.backgroundColor = stdcolor;
-        }
         float dirx = 0f, diry = 0f;
-        if( Mathf.Abs(player.transform.position.x - transform.position.x) > dimensionX){
-            if(player.transform.position.x > transform.position.x){
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) > dimensionX)
+        {
+            if (player.transform.position.x > transform.position.x)
+            {
                 dirx = -1.0f;
             }
             else
                 dirx = 1.0f;
         }
-        if( Mathf.Abs(player.transform.position.y - transform.position.y) > dimensionY){
-            if(player.transform.position.y > transform.position.y){
+        if (Mathf.Abs(player.transform.position.y - transform.position.y) > dimensionY)
+        {
+            if (player.transform.position.y > transform.position.y)
+            {
                 diry = -1.0f;
             }
             else
                 diry = 1.0f;
         }
-        transform.position += (Vector3.right *(-dirx) + Vector3.up *(-diry))* cameraSpeed * Time.deltaTime;
-        Debug.Log(transform.position + "   "+ player.transform.position);
+        transform.position += (Vector3.right * (-dirx) + Vector3.up * (-diry)) * cameraSpeed * Time.deltaTime;
+
     }
 }
---
+++ Scripts/hintProvider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class hintProvider : MonoBehaviour
+{
+    private Renderer rend;
+
+    private int hintsgiven;
+    public float hintNum;
+    public float hintTimeout = 1.5f;
+    public float preambleTimeout = 2.0f;
+    private float hinttime = -100.0f;
+    public Color hcolor;
+    private Color setcolor;
+    public Color stdcolor;
+    public int zoom;
+    public int stdzoom;
+    public Text hintdisplay;
+
+    //private float startTime = 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        setcolor = stdcolor;
+        dataHandler.LoadData();
+        hintNum = 1 + dataHandler.tmp.hintsLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (hintsgiven < hintNum)
+            {
+                setcolor = hcolor;
+                hinttime = Time.timeSinceLevelLoad;
+                hintsgiven += 1;
+                hintdisplay.text = (hintNum - hintsgiven) + " hints left";
+                
+                
+            }
+            else
+            {
+                Debug.Log("Consumed all hints");
+            }
+        }
+        dataHandler.tmp.hintsLeft = hintNum - hintsgiven;
+        dataHandler.SaveData();
+        if (Time.timeSinceLevelLoad < hinttime + hintTimeout)
+        {
+            Color c = Color.Lerp(setcolor, stdcolor, (Time.timeSinceLevelLoad - hinttime) / hintTimeout);
+            rend.material.color = c;
+            rend.material.SetColor("_EmissionColor", c);// = Color.Lerp(setcolor, stdcolor, (Time.timeSinceLevelLoad - timeOfCollision) / timeout);
+        }
+        else
+        {
+            hintdisplay.text = "";
+        }
+        
+    }
+}
