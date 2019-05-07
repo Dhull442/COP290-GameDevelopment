4111cbe79b520eca1913501d203e1d941269022a
Saksham Dhull <sakshamdhull442@gmail.com>
2019-05-07 21:19:40 +0530

    level files

--- Scripts/levelcompletion.cs
+++ Scripts/levelcompletion.cs
@@ -5,14 +5,6 @@ using UnityEngine.UI;
 public class levelcompletion : MonoBehaviour
 {
     public GameManagerScript gm;
-    public GameObject panel;
-    public float timeLimit;
-    private float startTime;
-    public Slider timebar;
-    public Image fill;
-
-    public Color endcolor;
-    public Color startcolor;
 
     void OnTriggerEnter(Collider coll){
         if(coll.gameObject.tag == "player"){
@@ -20,25 +12,4 @@ public class levelcompletion : MonoBehaviour
             gm.levelcomplete();
         }   
     }
-
-    void Start(){
-        Time.timeScale = 1;
-        startTime = Time.timeSinceLevelLoad;
-    }
-
-    void Update(){
-        if(Time.timeSinceLevelLoad > startTime+timeLimit){
-            Debug.Log("LEVEL FAILED");
-            gm.levelfailed();
-        }
-        float timeleft = timeLimit - (startTime + Time.timeSinceLevelLoad);
-        timebar.value = timeleft/timeLimit;
-        float value = (timeleft/timeLimit);
-        fill.color =  value * startcolor + (1 - value)  * endcolor ;
-    }
-
-    public void penalise(float time){
-        startTime += time;  
-    }
-
 }
---
+++ Scripts/nextlevelScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nextlevelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameManagerScript gm;
+    public void gotoNext()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+        gm.loadNextLevel();
+    }
+}
